00f03091a7df39bb36e558c5e7dc880ac086f7bf
Bruno Dias <bruno@outeredgeuk.com>
2015-08-20 15:17:50 +0100

    Code format adjustments

--- app/code/local/Edge/DeliveryInstructions/Model/Observer.php
+++ app/code/local/Edge/DeliveryInstructions/Model/Observer.php
@@ -5,7 +5,7 @@ class Edge_DeliveryInstructions_Model_Observer
     public function saveDeliveryInstructions($observer)
     {
         $message = strip_tags($observer->getEvent()->getRequest()->getParam('delivery_instructions'));
-        $quote = $observer->getEvent()->getQuote();
+        $quote   = $observer->getEvent()->getQuote();
 
         $quote->setDeliveryInstructions($message);
 
